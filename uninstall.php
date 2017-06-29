309216f39ca3214f6872bf768245ed3b913ddc10
informatux45 <informatux45@gmail.com>
2017-06-29 14:37:09 +0200

    Uninstall process sample

--- uninstall.php
+++ uninstall.php
@@ -1,19 +1,62 @@
 <?php
+/**
+ * Runs on Uninstall of ICustomizer
+ *
+ * @package     ICustomizer
+ * @author      INFORMATUX
+ * @copyright   2017 INFORMATUX
+ * @license     GPL-3.0+
+ * @link      	https://informatux.com/category/WORDPRESS
+ */
 
+// ------------------------------------------------
 // if uninstall.php is not called by WordPress, die
+// ------------------------------------------------
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
- 
-//$option_name = 'wporg_option';
-// 
-//delete_option($option_name);
-// 
-//// for site options in Multisite
+// ------------------------------------------------
+
+// -------------- 
+// Delete Options
+// --------------
+//$options =[
+//	'rhprint_text',
+//	'rhprint_icon',
+//	'rhprint_comments',
+//	'rhprint_message',
+//];
+// --------------
+//foreach ( $options as $option ) {
+//	if ( get_option( $option ) ) {
+//		delete_option( $option );
+//	}
+//}
+// --------------
+
+// ------------
+// Delete Pages
+// ------------
+//wp_trash_post( 101 );
+// ------------
+
+// -----------------------------
+// Delete Custom Post Type posts
+// -----------------------------
+//$wpdb->query( "DELETE FROM {$wpdb->posts} WHERE post_type IN ( 'my_custom_post_type' );" );
+//$wpdb->query( "DELETE FROM {$wpdb->postmeta} meta LEFT JOIN {$wpdb->posts} posts ON posts.ID = meta.post_id WHERE wp.ID IS NULL;" );
+// -----------------------------
+
+// -----------------------------
+// for site options in Multisite
+// -----------------------------
 //delete_site_option($option_name);
-// 
-//// drop a custom database table
+// -----------------------------
+
+// ----------------------------
+// drop a custom database table
+// ----------------------------
 //global $wpdb;
 //$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}mytable");
-
+// ----------------------------
 ?>
